d1287b192b6442970421579d04d6b32d50098de0
HeoKhoDai <baokhanh20802@gmail.com>
2025-03-31 22:50:41 +0700

    navigating

--- app/page.js
+++ app/page.js
@@ -1,3 +1,12 @@
+import Link from "next/link";
+import Navigation from "./components/Navigation";
+
 export default function Page() {
-  return <h1>Piu Cabins. Welcome to paradise</h1>;
+  return (
+    <div>
+      <Navigation />
+      <h1>Piu Cabins. Welcome to paradise</h1>;
+      <Link href="/cabins">Explore luxury cabins</Link>
+    </div>
+  );
 }
