3967e05b376b5b5281995c02553dafa1ecf5b239
HeoKhoDai <baokhanh20802@gmail.com>
2025-04-01 17:24:28 +0700

    creating a layout

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,10 +1,7 @@
-import Navigation from "../components/Navigation";
-
 export default function Page() {
   return (
     <div>
-      <Navigation />
-      <h1>Cabins page</h1>;
+      <h1>Cabins page</h1>
     </div>
   );
 }
